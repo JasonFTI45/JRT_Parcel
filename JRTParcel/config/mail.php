6c934fc1e619757ff3e94c5c429c6e95c1ecf799
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-06-07 14:45:00 +0700

    Added mail

--- JRTParcel/config/mail.php
+++ JRTParcel/config/mail.php
@@ -98,8 +98,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'JRTParcel@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'JRTParcel'),
     ],
 
     /*
