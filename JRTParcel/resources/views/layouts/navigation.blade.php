8b7e3de75dfa5384365d6089261c2b1ba0161a2d
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-06-08 20:51:07 +0700

    Resi Page

--- JRTParcel/resources/views/layouts/navigation.blade.php
+++ JRTParcel/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('resi')" :active="request()->routeIs('resi')">
+                        {{ __('Resi') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
