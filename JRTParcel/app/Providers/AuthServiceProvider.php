c5762985d9d4c8ac1b17c202081f280a2d714619
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-06-08 20:24:23 +0700

    Added missing import

--- JRTParcel/app/Providers/AuthServiceProvider.php
+++ JRTParcel/app/Providers/AuthServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use Illuminate\Auth\Notifications\ResetPassword;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
