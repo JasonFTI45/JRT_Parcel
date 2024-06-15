508e57586e0840940be0da3c240163fba6df5758
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-06-15 23:50:23 +0700

    change route of resi

--- JRTParcel/resources/views/layouts/navigation.blade.php
+++ JRTParcel/resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('resi')" :active="request()->routeIs('resi')">
+                    <x-nav-link :href="route('resi.index')" :active="request()->routeIs('resi.index')">
                         {{ __('Resi') }}
                     </x-nav-link>
                 </div>
