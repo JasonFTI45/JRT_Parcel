5a1d0269459c6bf6d55fdf98456019fa2d921cc8
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-06-15 23:50:53 +0700

    somewhat working database

---
+++ JRTParcel/database/migrations/2024_06_09_105255_create_resi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resi', function (Blueprint $table) {
+            $table->id();
+            $table->string('kodeResi');
+            $table->string('jenisPengiriman');
+            $table->unsignedBigInteger('penerima_id')->nullable();
+            $table->unsignedBigInteger('pengirim_id')->nullable();
+            $table->string('kecamatan_kota_asal');
+            $table->string('kecamatan_kota_tujuan');
+            $table->timestamps();
+
+            $table->foreign('penerima_id')->references('id')->on('penerima')->onDelete('set null');
+            $table->foreign('pengirim_id')->references('id')->on('pengirim')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resi');
+    }
+};
