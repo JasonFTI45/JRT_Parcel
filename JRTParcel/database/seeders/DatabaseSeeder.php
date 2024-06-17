a9d190a4f0293ed6152aa4342fe4fca7c10cff13
JasonFTI45 <jason.535220045@stu.untar.ac.id>
2024-06-17 15:00:53 +0700

    Added Admin Seed

--- JRTParcel/database/seeders/DatabaseSeeder.php
+++ JRTParcel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use database\seeders\AdminUserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(AdminUserSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
