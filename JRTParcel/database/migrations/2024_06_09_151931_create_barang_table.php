266c512e85bfd4c0aa194ebdd6721ca4851e59e1
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-06-19 22:59:46 +0700

    fix error on migrate

--- JRTParcel/database/migrations/2024_06_09_151931_create_barang_table.php
+++ JRTParcel/database/migrations/2024_06_09_151931_create_barang_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('resi_id');
             $table->string('tipe_komoditas');
-            $table->float('berat');
             $table->float('lebar');
             $table->float('panjang');
             $table->float('tinggi');
