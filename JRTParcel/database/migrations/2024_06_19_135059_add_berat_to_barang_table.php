dba3d5d1f633ccf7bdb3d4d53950b4a46167987f
JasonFTI45 <jason.535220045@stu.untar.ac.id>
2024-06-19 20:56:16 +0700

    Added migration for harga and berat

---
+++ JRTParcel/database/migrations/2024_06_19_135059_add_berat_to_barang_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBeratToBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            $table->integer('berat')->default(0)->after('tipe_komoditas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            $table->dropColumn('berat');
+        });
+    }
+}
--- JRTParcel/resources/views/dashboard.blade.php
+++ JRTParcel/resources/views/dashboard.blade.php
@@ -6,12 +6,19 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+                <div class="flex flex-wrap -mx-3">
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        Hello, {{ Auth::user()->name }}!
+                    </div>
+                    <div class="w-full md:w-1/2 px-3">
+                        Hello, {{ Auth::user()->name }}!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </x-app-layout>
