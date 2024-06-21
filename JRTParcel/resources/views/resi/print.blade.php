ad35da754c5f84a83839d505d7a655740d3312ec
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-06-21 21:30:47 +0700

    number format

--- JRTParcel/resources/views/resi/print.blade.php
+++ JRTParcel/resources/views/resi/print.blade.php
@@ -61,7 +61,7 @@
             </table>
         </div>
         <div class="total-resi">
-            <h1 style="font-weight: bolder;">{{ __('Biaya Pengiriman : ') . $resi->harga }}</h1>
+            <h1 style="font-weight: bolder;">{{ __('Biaya Pengiriman : ') . 'Rp ' . number_format($resi->harga, 2, ',', '.') }}</h1>
         </div>
     </div>
 
