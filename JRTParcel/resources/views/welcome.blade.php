b535a5bc2affd70fa88917a7d65aa41036ea030f
JasonFTI45 <jason.535220045@stu.untar.ac.id>
2024-06-25 20:17:32 +0700

    Made dashboard button when logged in welcome page have the same style as login button

--- JRTParcel/resources/views/welcome.blade.php
+++ JRTParcel/resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
         <div class="container-navbar">
             <div>
                 @auth
-                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                <a href="{{ url('/dashboard') }}" class="btn-1 bolder">Dashboard</a>
                 @else
                 <a href="{{ route('login') }}" class="btn-1 bolder">Login</a>
                 @endauth
